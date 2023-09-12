693f1236fa029a2475d98658113717661b49b209
Rizky Sanjaya T <sanjayatandia@gmail.com~>
2023-09-12 10:58:02 +0700

    postgre sql connection update

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 from app import app
 from flaskext.mysql import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'bookapi'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+
+# # Configure the PostgreSQL database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/bookapi'
+
+# # Optional: Disable SQLAlchemy track modifications
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# # Create an SQLAlchemy database object
+# db = SQLAlchemy(app)
+# db.init_app(app)
+
+# mysql = MySQL()
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = 'bookapi'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
--- main.py
+++ main.py
@@ -1,42 +1,66 @@
 import pymysql
+import psycopg2
+from psycopg2 import extras
 from app import app
-from config import mysql
+# from config import mysql
 from flask import jsonify
 from flask import flash, request, render_template, redirect, url_for
 
-@app.route('/create', methods=['POST'])
-def add_book():
-    try:
-        _title = request.form['title']
-        _author = request.form['author']
-        _genre = request.form['genre']
+db_params = {
+    'host': '103.157.46.37',
+    'port': '5432',  # PostgreSQL default port is 5432
+    'database': 'example_app',
+    'user': 'dev1234',
+    'password': 'Dev123'
+}
 
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("INSERT INTO books(title, author, genre) VALUES(%s, %s, %s)", (_title, _author, _genre))
-        conn.commit()
 
-        # flash('Book added successfully!', 'success')  # Optional: Flash a success message
 
-        return redirect(url_for('book'))
-    except Exception as e:
-        # Handle exceptions here (e.g., database errors)
-        # flash('An error occurred while adding the book.', 'error')
-        return redirect(url_for('book'))  # Redirect to the book page even if there's an error
-    finally:
-        cursor.close()
-        conn.close()
-        
-#insert book page
+# View all books 
+
+@app.route('/')
+def book():
+    
+        # conn = mysql.connect()
+        # cursor = conn.cursor(pymysql.cursors.DictCursor)
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM books")
+    bookRows = cursor.fetchall()
+    return render_template('books.html', title='Books', books=bookRows)
+    
+
+# Add book page
 @app.route('/new_book')
 def new_book():
     return render_template('add-book.html')
 
+# Insert book function
+@app.route('/create', methods=['POST'])
+def add_book():
+    # try:
+        _title = request.form['title']
+        _author = request.form['author']
+        _genre = request.form['genre']
+
+        # conn = mysql.connect()
+        # cursor = conn.cursor(pymysql.cursors.DictCursor)
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute("INSERT INTO books(title, author, genre) VALUES(%s, %s, %s)", (_title, _author, _genre))
+        conn.commit()
+
+        return redirect(url_for('book'))
+
+        
+
+
 #update book page and display data from coreesponding id
+
 @app.route('/edit/<string:id>', methods=['GET', 'POST'])
 def edit(id):
-    conn = mysql.connect()
-    cursor = conn.cursor()
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     if request.method == 'POST':
         # Retrieve the form data
@@ -52,7 +76,7 @@ def edit(id):
         conn.commit()
 
         # flash('Book updated successfully!', 'success')
-        conn.close()
+        # conn.close()
         return redirect(url_for('book'))
 
     # If the request method is GET, retrieve the data of the book to be edited
@@ -65,6 +89,19 @@ def edit(id):
     else:
         # flash('Book not found!', 'danger')
         return redirect(url_for('book'))
+    
+
+# Delete book function
+@app.route('/delete/<string:id>', methods=['GET'])
+def delete_book(id):
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("DELETE FROM books WHERE id = %s", (id,))
+    conn.commit()
+    # flash('Book deleted successfully!', 'success')
+    conn.close()
+    return redirect(url_for('book'))
+
    
 # #update function
 # @app.route('/update', methods=['POST'])
@@ -89,36 +126,24 @@ def edit(id):
 #         conn.close()
 #         return redirect(url_for('book'))
     
-@app.route('/book')
-def book():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM books")
-        bookRows = cursor.fetchall()
-        return render_template('books.html', title='Books', books=bookRows)
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close() 
-        conn.close()
 
+# View Data By ID (Not Used)
 
-@app.route('/book/<int:id>')
-def book_details(id):
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT id, title, author, genre FROM books WHERE id =%s", (id,))
-        bookRow = cursor.fetchone()
-        respone = jsonify(bookRow)
-        respone.status_code = 200
-        return respone
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close() 
-        conn.close() 
+# @app.route('/book/<int:id>')
+# def book_details(id):
+#     try:
+#         conn = mysql.connect()
+#         cursor = conn.cursor(pymysql.cursors.DictCursor)
+#         cursor.execute("SELECT id, title, author, genre FROM books WHERE id =%s", (id,))
+#         bookRow = cursor.fetchone()
+#         respone = jsonify(bookRow)
+#         respone.status_code = 200
+#         return respone
+#     except Exception as e:
+#         print(e)
+#     finally:
+#         cursor.close() 
+#         conn.close() 
 
 # @app.route('/update', methods=['PUT'])
 # def update_book():
@@ -139,30 +164,18 @@ def book_details(id):
 #         cursor.close() 
 #         conn.close() 
 
-@app.route('/delete/<int:id>', methods=['GET'])
-def delete_book(id):
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute("DELETE FROM books WHERE id =%s", (id,))
-		conn.commit()
-		return redirect(url_for('book'))
-	except Exception as e:
-		return redirect(url_for('book'))
-	finally:
-		cursor.close() 
-		conn.close()
+
         
-       
-@app.errorhandler(404)
-def showMessage(error=None):
-    message = {
-        'status': 404,
-        'message': 'Record not found: ' + request.url,
-    }
-    respone = jsonify(message)
-    respone.status_code = 404
-    return respone
+# # Error Handling (Not Used)       
+# @app.errorhandler(404)
+# def showMessage(error=None):
+#     message = {
+#         'status': 404,
+#         'message': 'Record not found: ' + request.url,
+#     }
+#     respone = jsonify(message)
+#     respone.status_code = 404
+#     return respone
         
 if __name__ == "__main__":
     app.run(debug=True)
